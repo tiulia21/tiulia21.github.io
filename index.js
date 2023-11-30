a7acf0981aa216afcccb72deb66271bb7b17b5fa
Todea Iulia <iulia21ana@gmail.com>
2023-11-30 03:50:58 -1000

    first version

---
+++ index.js
@@ -0,0 +1,20 @@
+function showHome() {
+  var oldPage = document.getElementById("showhome");
+  oldPage.style.display = "none";
+   var oldPage = document.getElementById("showhome");
+  oldPage.style.display = "none";
+function showSkills() {
+  var oldPage = document.getElementById("projects");
+  oldPage.style.display = "none";
+
+  var page = document.getElementById("Skills");
+  page.style.display = "block";
+}
+
+function showLanguages() {
+  var oldPage = document.getElementById("projects");
+  oldPage.style.display = "none";
+
+  var page = document.getElementById("Languages");
+  page.style.display = "block";
+}
