f49ce37be4d3cf74f17184080735135e886e211a
Todea Iulia <iulia21ana@gmail.com>
2023-12-04 08:31:27 -1000

    simplify code

--- index.js
+++ index.js
@@ -1,36 +1,43 @@
+function show(id) {
+  console.info("show", id);
+  var page = document.getElementById(id);
+  console.info("page", page);
+  page.style.display = "block";
+}
+
 function hide(id) {
   console.info("hide", id);
-  document.getElementById("id").style.display = "none";
+  document.getElementById(id).style.display = "none";
 }
+
 function showHome() {
-  hide("skills");
-  hide("projects");
-  hide("languages");
+  hide("Skills");
+  hide("Projects");
+  hide("Languages");
 
-  var page = document.getElementById("home");
-  page.style.display = "block";
+  show("Home");
 }
+
 function showSkills() {
-  hide("home");
-  hide("project");
-  hide("langauges");
+  hide("Home");
+  hide("Project");
+  hide("Langauges");
 
-  var page = document.getElementById("skills");
-  page.style.display = "block";
+  show("Skills");
 }
+
 function showProjects() {
-  hide("home");
-  hide("skills");
-  hide("languages");
+  hide("Home");
+  hide("Skills");
+  hide("Languages");
 
-  var page = document.getElementById("projects");
-  page.style.display = "block";
+  show("Projects");
 }
-function showLanguages() {
-  hide("home");
-  hide("skills");
-  hide("projects");
 
-  var page = document.getElementById("languages");
-  page.style.display = "block";
+function showLanguages() {
+  hide("Home");
+  hide("Skills");
+  hide("Projects");
+
+  show("Languages");
 }
