5fdde10a7a35e6bed0eb9e19c7dcfed1a041eaca
Todea Iulia <iulia21ana@gmail.com>
2023-12-04 04:34:54 -1000

    update job title with js

--- examples.js
+++ examples.js
@@ -12,10 +12,13 @@ function variableExamples() {
 
   var skills = ["html", "css"];
   console.debug("tipul variabilei skills", typeof skills);
-
+}
+function updateTitle(title) {
   var job = document.getElementById("job-title");
   console.warn("job", job);
   console.info(typeof job);
+  job.innerHTML = title;
 }
 
 variableExamples();
+updateTitle(" Web Developer @RWS");
