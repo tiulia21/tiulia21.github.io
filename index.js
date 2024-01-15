80748b575171964744b559e74be330f393ea0565
Todea Iulia <iulia21ana@gmail.com>
2024-01-15 09:06:29 -1000

    add $ shortcut function

--- index.js
+++ index.js
@@ -2,6 +2,13 @@
 var activePage = "skills";
 
 // functii publice
+function $(selector) {
+  console.info("gaseste elementul: %o", selector);
+
+  var el = $(selector);
+  console.warn("elemntul gasit este:", el);
+  return el;
+}
 function hide(id) {
   console.info("hide", id);
   document.getElementById(id).style.display = "none";
@@ -16,18 +23,18 @@ function show(id) {
 
 function showPage(id) {
   console.info("show page", id);
-  var prevLink = document.querySelector("a[data-page=" + activePage + "]");
+  var prevLink = $("a[data-page=" + activePage + "]");
   prevLink.classList.remove("active");
   hide(activePage);
 
-  var nextLink = document.querySelector(`a[data-page=${id}]`);
+  var nextLink = $(`a[data-page=${id}]`);
   nextLink.classList.add("active");
   show(id);
   activePage = id;
 }
 
 function initEvents() {
-  var toolbar = document.querySelector("#top-menu-bar");
+  var toolbar = $("#top-menu-bar");
   toolbar.addEventListener("click", function (e) {
     if (e.target.matches("a")) {
       var page = e.target.dataset.page;
@@ -38,7 +45,7 @@ function initEvents() {
 }
 
 function showskills() {
-  var ul = document.querySelector("#skills ul");
+  var ul = $("#skills ul");
   console.log("show skills");
   var skills = [
     {
